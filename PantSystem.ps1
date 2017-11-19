ed91de49a34f7170f4bd86fb0389bae2dfaa1303
Daniel Søgaard <nothingisrealmedia@hotmail.com>
2017-11-19 21:13:53 +0100

    Add files via upload

---
+++ PantSystem.ps1
@@ -0,0 +1,45 @@
+﻿cls
+#sætter variabler
+    [int]$bill;
+    $pant = @{"a"=1; "b"=1.5; "c"=3}
+
+
+    $pant.GetEnumerator() | % {
+    
+        write-host $_.Key " = " $_.Value "kr"
+    
+    }
+
+#List alle symbolerne, og tryk derefter {enter}
+
+$playground = Read-Host "PantSymboler";
+
+
+#udregner alle symboler
+$test = $playground -Split "";
+
+
+        $test | foreach {
+    
+            if ($_ -eq "a"){
+    
+                $bill += 1
+
+            } elseif ($_ -eq "b"){
+    
+                $bill += 1.5
+
+            } elseif ($_ -eq "c"){
+
+                $bill += 3
+
+            }
+    
+        }
+
+
+write-host "Total " -nonewline
+write-host $bill -f Green -nonewline
+Write-Host " kr"
+
+$bill = $null;
